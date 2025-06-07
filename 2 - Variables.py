cd5c5943b4393c062f00d81ce448f8ea490b20b1
Eduardo Rojas <prof.eduardo17@gmail.com>
2025-06-07 02:11:52 -0300

    commit inicial

---
+++ 2 - Variables.py
@@ -0,0 +1,8 @@
+print("Esto es una suma")
+
+num_uno = 2
+num_dos = 3
+
+resultado = num_uno + num_dos
+
+print("El resultado de tu suma es: " , resultado)
---
+++ 3 - Cadena de caracteres.py
@@ -0,0 +1,19 @@
+##Asignación
+##Usa el signo "+="
+
+mensaje = "Hola"
+mensaje += ", "
+mensaje += "Eduardo"
+
+print(mensaje)
+
+##Concatenación
+##Usa el signo "+"
+
+mensaje = "Hola"
+espacio = ", "
+nombre = "Eduardo"
+
+print(mensaje+espacio+nombre)
+
+

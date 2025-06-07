2c0c36d6c505c9f40d5dd5f5d763958b038c8e25
Eduardo Rojas <prof.eduardo17@gmail.com>
2025-06-07 02:24:39 -0300

    commit inicial

--- 3 - Cadena de caracteres.py
+++ 3 - Cadena de caracteres.py
@@ -1,6 +1,8 @@
 ##Asignación
 ##Usa el signo "+="
 
+print("Asignación")
+
 mensaje = "Hola"
 mensaje += ", "
 mensaje += "Eduardo"
@@ -10,10 +12,26 @@ print(mensaje)
 ##Concatenación
 ##Usa el signo "+"
 
+print("Concatenación")
+
 mensaje = "Hola"
 espacio = ", "
 nombre = "Eduardo"
 
 print(mensaje+espacio+nombre)
+
+##Convertir a caracter
+## x = str(x)
+
+print("Convertir a caracter")
+
+numero_uno = 4
+numero_dos = 6
+resultado  = numero_uno + numero_dos
+resultado  = str(resultado)
+
+print("El resultado de tu suma es:" + resultado)
+
+
 
 
